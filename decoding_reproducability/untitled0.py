d1f8dab2cc84819672d055cd49fe8763a2efbfa8
Lena-Kemmelmeier <lkemmelmeier@ucsd.edu>
2025-11-03 15:05:58 -0800

    test

---
+++ decoding_reproducability/untitled0.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  3 14:43:25 2025
+
+@author: lenakemmelmeier
+"""
+
+#%% Set up environment - load in fMRI data for each participant
+
+
+
+
+
+
+#%% Conduct decoding analysis
+
+
+
+
+
+
+
+#%% run ANOVA (maybe do this in R)
