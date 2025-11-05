b874637b4009ac70f19723676bc36e21df38779a
Lena-Kemmelmeier <lkemmelmeier@ucsd.edu>
2025-11-04 19:19:26 -0800

    making helper function to load in bold data

--- decoding_reproducability/reproducing_decoding_anova_result.py
+++ decoding_reproducability/reproducing_decoding_anova_result.py
@@ -11,38 +11,78 @@ Created on Mon Nov  3 14:43:25 2025
 import numpy as np
 import os, sys # used for configuring paths
 import pandas as pd
-import sklearn
-import sklearn.svm, sklearn.discriminant_analysis, sklearn.linear_model
-import time
-from joblib import effective_n_jobs
-import datetime
+
+# actual stats packages
 import scipy.stats
-
-
-# notes to self -- do not need dprime; just getting decoding acc, not d-prime
-# don't need any special helper functions
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.model_selection import LeaveOneGroupOut
+from statsmodels.stats.anova import AnovaRM
 
 
 #%% Init variables + set paths
 
-# note to self: change this below so it can read where the repo got cloned into
-# also... should probably not be loading the data onto git
-# I should add into the ReadMe where people can go download this instead of posting their data
-bold_data_path = '/Users/lena/Documents/GitHub/henderson2025/bold_data_henderson2025/' # add the path with the original data to path
-sys.path.append(bold_data_path)
+# set paths (I used os package since it's platform independent -- easier for someone to reuse)
+root_dir = '/Users/lena/Documents/GitHub/henderson2025' # root_dir for the repo
+bold_data_path = os.path.join(root_dir, 'bold_data_henderson2025') # path containing downloaded data from Henderson et al.
+stats_output_path = os.path.join(root_dir, 'bold_decoding_anova_results') # output path for stats CSVs
+sys.path.append(root_dir) # add the repo to the path
 
 num_participants = 10 # number of participants
-sub_ids = [str(i) for i in range(1, num_participants + 1)] # uses list comprehension to make a list of sub_ids to iterate over later
-sub_ids_list = np.array(sub_ids)
+
+# uses list comprehension to make a list of sub IDs to iterate over later
+sub_ids = [str(i).zfill(2) for i in range(1, num_participants + 1)] # zero pads that sub IDs up to 2 digits (e.g., '1' becomes '01')
+sub_ids_list = np.array(sub_ids)  
+
+boundaries = {"linear1": [[1, 4], [2, 3]], "linear2": [[1, 2], [3, 4]], "nonlinear": [[1, 3], [2, 4]]} # dict for which quartiles correspond to which boundary groupings in the different conditions
+
 
 #%% Helper functions (run this before the cells below!)
 
-# define a function to load the main task bold data in
-def load_bold_main_data(sub_id, data_path, ):
+# define a function to load the bold data
+def load_bold_main_data(sub_id, data_path):
+    """
+    loads the trial-averaged bold data and labels for each participant (labels are from the behavioral CSVs)
     
-    # load the 
+    returns
+    -------
+    main_data_list: list of length num_rois
+        each item stored is a 2d float array (num_trials x num_voxels) of bold activity per ROI
+        these data are z-scored within the run and averaged over TR window
+    main_labels: pandas df
+        contains columns ['run_overall','is_main_grid','quadrant','task']
+    roi_names: list of length num_rois
+        contains ROI names (strings) that align with the order of main_data_list
+    """
+    
+    # read in the labels from the behavioral data
+    behavioral_data_path = os.path.join(data_path, f"DataBehavior/S{sub_id}", f"S{sub_id}_maintask_preproc_all.csv") # this is the path to each individual participant's behavioral data file
+    main_labels = pd.read_csv(behavioral_data_path, index_col=0)
+    
+    # get the bold data (we converted this from a mat file to a npz file earlier)
+    npz_path = os.path.join(data_path, f"S{sub_id.zfill(2)}_main_data.npz")
 
+    # npz should contain: roi_names (list[str]) and arrays keyed by roi name
+    packed = np.load(npz_path, allow_pickle=True)
+    roi_names = list(packed["roi_names"])
+    main_data_list = [packed[roi] for roi in roi_names]
+
+    # so peeking at the original pipeline, the authors mean center the data (subtract per-trial voxel mean)
+    for i in range(len(main_data_list)):
+        roi_bold_data = main_data_list[i].astype(float, copy=False)
+        main_data_list[i] = roi_bold_data - roi_bold_data.mean(axis=1, keepdims=True)
+
+    return main_data_list, main_labels, roi_names
+
+ 
 #%% Load in fMRI data
+
+
+
+
+
+
+
+
 
 
 
